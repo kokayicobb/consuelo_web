388788927a58fa74c4018d93cf0b15bbba210bc4
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2024-11-21 04:08:09 -0500

    lets see

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,6 +7,12 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="The personalized virtual fitting solution for Ecommerce retailers. Our innovative SaaS platform enables customers to try on clothes online ensuring the perfect fit and reducing returns." />
       <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link
+  rel="icon"
+  href="/icon?<generated>"
+  type="image/<generated>"
+  sizes="<generated>"
+/>
     </>
   );
 }
