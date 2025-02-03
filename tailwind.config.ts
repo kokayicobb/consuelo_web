a37df455d119db16976f36ae7b912fa6e127c093
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2025-02-03 17:03:21 -0500

    Pricing card update, Team section, Hero section, Pitch Deck

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,79 +11,92 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))'
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))'
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))'
-        },
-        foreground: 'hsl(var(--foreground))',
-        background: 'hsl(var(--background))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))'
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))'
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))'
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))'
-        },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-      },
-      boxShadow: {
-        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
-      },
-      keyframes: {
-        'orb-move': {
-      '0%, 100%': { transform: 'translateY(0) scale(1)' },
-      '50%': { transform: 'translateY(-20px) scale(1.05)' },
-        },
-        'subtle-pulse': {
-          '0%, 100%': { opacity: '0.5' },
-          '50%': { opacity: '0.4' },
-        },
-        float: {
-          '0%, 100%': { transform: 'translateY(0)' },
-          '50%': { transform: 'translateY(-20px)' },
-        },
-        shimmer: {
-          '0%': {
-            transform: 'translateX(-100%)'
-          },
-          '100%': {
-            transform: 'translateX(100%)'
-          }
-        }
-      },
-      animation: {
-        'very-subtle-pulse': 'subtle-pulse 12s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        shimmer: 'shimmer 1.5s ease-in-out infinite'
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)'
-      }
-      
-    }
-    
+  	extend: {
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			foreground: 'hsl(var(--foreground))',
+  			background: 'hsl(var(--background))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))'
+  		},
+  		boxShadow: {
+  			input: '`0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`'
+  		},
+  		keyframes: {
+  			'orb-move': {
+  				'0%, 100%': {
+  					transform: 'translateY(0) scale(1)'
+  				},
+  				'50%': {
+  					transform: 'translateY(-20px) scale(1.05)'
+  				}
+  			},
+  			'subtle-pulse': {
+  				'0%, 100%': {
+  					opacity: '0.5'
+  				},
+  				'50%': {
+  					opacity: '0.4'
+  				}
+  			},
+  			float: {
+  				'0%, 100%': {
+  					transform: 'translateY(0)'
+  				},
+  				'50%': {
+  					transform: 'translateY(-20px)'
+  				}
+  			},
+  			shimmer: {
+  				'0%': {
+  					transform: 'translateX(-100%)'
+  				},
+  				'100%': {
+  					transform: 'translateX(100%)'
+  				}
+  			}
+  		},
+  		animation: {
+  			'very-subtle-pulse': 'subtle-pulse 12s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+  			shimmer: 'shimmer 1.5s ease-in-out infinite'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		maskImage: {
+  			'radial-gradient': 'radial-gradient(var(--tw-gradient-stops))'
+  		}
+  	}
   },
   
   plugins: [
