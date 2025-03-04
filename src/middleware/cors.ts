f9d098bcfa214754d016b33829e71da7759b531b
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2025-03-03 22:53:41 -0500

    Cors open

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,7 +2,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://try-on-testing.myshopify.com',
+  'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
   'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
