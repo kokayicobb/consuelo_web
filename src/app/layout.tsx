1053340390e0b4867d16bd76413d312dd1fbd7f7
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2025-06-17 02:31:18 -0400

    accounts done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body> 
+        
       <AuthProvider>
         <Providers>
         <Toaster /> 
