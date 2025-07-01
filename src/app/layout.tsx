e69594c8f91c51404252cfb6b64d4650d693610f
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2025-07-01 04:47:31 -0400

    yikes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
       
       <head>
+        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
