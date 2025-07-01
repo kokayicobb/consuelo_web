2727cb41ad4cfb39826f59a2e3e4b94e00e3cd8a
kokayicobb <81458034+kokayicobb@users.noreply.github.com>
2025-07-01 04:38:28 -0400

    worklkkl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
+      
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
